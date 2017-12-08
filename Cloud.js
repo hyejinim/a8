b04d95d0e6e114f48de4ecef3e96d5bd6b038485
hyejinim <hyejinim17@gmail.com>
2017-12-08 17:40:56 -0500

    Update Cloud.js

--- Cloud.js
+++ Cloud.js
@@ -1,24 +1,37 @@
-<!-- Class -->
 class Cloud {
   constructor() {
-    this.emoji = '☁️';
-    this.size = getRandomValue(40, 70);
+    // this.emoji = '☁️';
+    // this.size = getRandomValue(40, 70);
     this.x = Math.random() * c.width;
     this.y = Math.random() * (c.height/3);
+    this.image = new Image();
+    this.image.src = 'images/cloud.png';
+
   }
   draw(context) {
-    context.font = (this.size) + "px Helvetica";
-    context.fillText(this.emoji, this.x, this.y);
+    // context.font = (this.size) + "px Helvetica";
+    // context.fillText(this.emoji, this.x, this.y);
+
+    // For initial drawing
+    let x = this.x;
+    let y = this.y;
+    let image = this.image;
+    image.onload = function(){
+      context.drawImage(image, x, y, 75, 40);
+    }
+    // For animation
+    context.drawImage(image, x, y, 75, 40);
   }
   setPosition(x, y) {
     this.x = x;
     this.y = y;
   }
   tick() {
-    if (this.x < c.width) {
+
+    if (this.x > -50) {
     this.x = this.x - 1;
     } else {
-    this.x = 0;
+    this.x = c.width;
     }
   }
 }
