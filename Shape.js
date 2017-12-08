7d89c2751b1e5e9e4f1a07903f22d49340af19fd
hyejinim <hyejinim17@gmail.com>
2017-12-08 17:46:35 -0500

    Update Shape.js

--- Shape.js
+++ Shape.js
@@ -1,10 +1,12 @@
 class Shape {
   constructor(type) {
     this.type = type;
-    this.size = getRandomValue(70, 150);
+    this.size = getRandomValue(70, 150); // radious for circle, width/height for rect
+    // this.width = getRandomValue(70, 150);
+    // this.height = getRandomValue(70, 150);
     this.speed = this.size / 20;
     this.x = c.width + 50;
-    this.y = (c.height-15)*2/3;
+    this.y = c.height * 2/3;
     this.color = getRandomColor();
     // console.log("make shape " + runningShapes.length);
   }
@@ -24,6 +26,10 @@ class Circle extends Shape {
   constructor() {
     super();
     this.type = "circle";
+    // this.x = c.width + 50;
+    this.y = c.height * 2.25/3;
+    this.width = this.size; // for bounding box
+    this.height = this.size; // for bounding box
     // this.w = getRandomValue(10, 20);
     // console.log("make circle " + runningShapes.length);
   }
