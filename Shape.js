95cbd435965bf247f77f3e2603e83b39bc66378c
hyejinim <hyejinim17@gmail.com>
2017-12-09 03:14:36 -0500

    Update Shape.js

--- Shape.js
+++ Shape.js
@@ -6,7 +6,7 @@ class Shape {
     // this.height = getRandomValue(70, 150);
     this.speed = this.size / 20;
     this.x = c.width + 50;
-    this.y = c.height * 2/3;
+    this.y = c.height * 3/5;
     this.color = getRandomColor();
     // console.log("make shape " + runningShapes.length);
   }
@@ -20,6 +20,7 @@ class Shape {
   tick() {
     this.x -= 10;
   }
+
 }
 
 class Circle extends Shape {
@@ -27,25 +28,36 @@ class Circle extends Shape {
     super();
     this.type = "circle";
     // this.x = c.width + 50;
-    this.y = c.height * 2.25/3;
-    this.width = this.size; // for bounding box
-    this.height = this.size; // for bounding box
+    this.y = c.height * 3/5 + 50; // Adjust circle's y position
+    this.r = this.size/2;
     // this.w = getRandomValue(10, 20);
     // console.log("make circle " + runningShapes.length);
   }
   draw(context) {
     gc.fillStyle = this.color;
     context.beginPath();
-    context.arc (this.x, this.y, this.size/2, 0, Math.PI * 2);
+    context.arc (this.x, this.y, this.r, 0, Math.PI * 2);
     context.fill();
     // console.log("draw circle " + runningShapes.length);
   }
+  contains(px, py) {
+    console.log("circle contains")
+    if (Math.pow(px-this.x, this.r) + Math.pow(py - this.y, this.r) >= this.r * this.r) {
+      console.log("-----------------------Circle");
+      return true;
+    }
+    return false;
+  }
+
 }
 
 class Rectangle extends Shape {
   constructor() {
     super();
     this.type = "rectangle";
+    this.y = c.height * 3/5 + 15;
+    this.width = this.size; // for bounding box
+    this.height = this.size; // for bounding box
     // this.width = getRandomValue(100, 200);
     // this.height = getRandomValue(100, 200);
     // console.log("make rect " + runningShapes.length);
@@ -55,5 +67,13 @@ class Rectangle extends Shape {
     context.beginPath();
     context.fillRect(this.x, this.y, this.size, this.size);
     // console.log("draw rect " + runningShapes.length);
+  }
+  contains(px, py) {
+    console.log("rectan contains")
+    if ((this.x <= px && px <= this.x + this.width) && (this.y <= py && py <= this.y + this.height)) {
+      console.log("-----------------------Rect");
+      return true;
+    }
+    return false;
   }
 }
