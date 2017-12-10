cf2168d568d09e4adade3c15f0363990bcd099aa
hyejinim <hyejinim17@gmail.com>
2017-12-10 02:31:20 -0500

    Update Shape.js

--- Shape.js
+++ Shape.js
@@ -1,57 +1,116 @@
-class Gun {
-
-  constructor(player, type) {
+class Shape {
+  constructor(type) {
     this.type = type;
-    this.text = type + " >>"; //▶
-    this.size = 30;
-    this.x = player.x + 200;
-    this.y = player.y + 120;
-    this.shooting = false;
-    this.width = gc.measureText(type).width;
-    this.height = gc.measureText(type).height;
-    // this.hit = false;
-  }
-  draw(context) {
-    context.fillStyle = "#000000";
-    context.font = (this.size) + "px Helvetica";
-    context.textAlign="right";
-    context.fillText(this.text, this.x, this.y);
+    this.size = getRandomValue(70, 150); // radious for circle, width/height for rect
+    // this.width = getRandomValue(70, 150);
+    // this.height = getRandomValue(70, 150);
+    this.speed = this.size / 20;
+    this.x = c.width + 50;
+    this.y = c.height * 3/5;
+    this.color = getRandomColor();
+    // console.log("make shape " + runningShapes.length);
   }
   setPosition(x, y) {
     this.x = x;
     this.y = y;
   }
-  setShooting(isShot) {
-    this.shooting = isShot;
-    // this.hit = true;
-    // console.log();
+  setSize(x) {
+    this.size = x;
   }
   tick() {
-    if (this.shooting) {
-      // if (!this.hit) {
-        this.x += 20;
-        // console.log('shot tick')
-        // this.shot = false;
-      }
-      else {
+    this.x -= 20;
+  }
+}
 
-      }
+class Circle extends Shape {
+  constructor() {
+    super();
+    this.type = "circle";
+    // this.x = c.width + 50;
+    this.y = c.height * 3/5 + 50; // Adjust circle's y position
+    this.r = this.size/2;
+    // this.w = getRandomValue(10, 20);
+    // console.log("make circle " + runningShapes.length);
+  }
+  draw(context) {
+    gc.fillStyle = this.color;
+    context.beginPath();
+    context.arc (this.x, this.y, this.r, 0, Math.PI * 2);
+    context.fill();
+    // console.log("draw circle " + runningShapes.length);
+
+  }
+  contains(px, py) {
+    if (Math.sqrt((px-this.x)*(px-this.x) + (py-this.y)*(py-this.y)) < this.r) {
+      console.log("-----------------------Circle");
+      return true;
     }
+    return false;
   }
 
+}
 
-// if (this.jumped) {
-//   // Jumping animation
-//   if (this.y = (c.height-15) * 2.3/4) {
-//     this.image.src = 'images/monkey_jump_2.png';
-//     this.y = this.y - 300;
-//     console.log("player position: ", this.y);
-//   }
-//   this.jumped = false;
-// } else {
-//   if (this.y < (c.height-15) * 2.3/4) {
-//     this.y = this.y + 20;
-//     console.log("player position: ", this.y);
-//     this.image.src = 'images/monkey_jump_2.png';
-//   }
-// }
+class Rectangle extends Shape {
+  constructor() {
+    super();
+    this.type = "rectangle";
+    this.y = c.height * 3/5 + 15;
+    this.width = this.size; // for bounding box
+    this.height = this.size; // for bounding box
+    // this.width = getRandomValue(100, 200);
+    // this.height = getRandomValue(100, 200);
+    // console.log("make rect " + runningShapes.length);
+  }
+  draw(context) {
+    gc.fillStyle = this.color;
+    context.beginPath();
+    context.fillRect(this.x, this.y, this.size, this.size);
+    // console.log("draw rect " + runningShapes.length);
+  }
+  contains(px, py) {
+    // console.log("rectan contains")
+    if ((this.x <= px && px <= this.x + this.width) && (this.y <= py && py <= this.y + this.height)) {
+      console.log("-----------------------Rect");
+      return true;
+    }
+    return false;
+  }
+}
+
+class Triangle extends Shape {
+  constructor() {
+    super();
+    this.type = "triangle";
+    this.y = c.height * 3/5 + 15;
+    this.width = this.size; // for bounding box
+    this.height = this.size; // for bounding box
+    // this.width = getRandomValue(100, 200);
+    // this.height = getRandomValue(100, 200);
+    // console.log("make rect " + runningShapes.length);
+  }
+  draw(context) {
+    gc.fillStyle = this.color;
+    // context.beginPath();
+    // context.fillRect(this.x, this.y, this.size, this.size);
+
+    context.beginPath();
+    context.moveTo(100, 100);
+    context.lineTo(100, 300);
+    context.lineTo(300, 300);
+    context.closePath();
+
+    // the outline
+    context.lineWidth = 10;
+    context.strokeStyle = '#666666';
+    context.stroke();
+
+    context.fill();
+  }
+  contains(px, py) {
+    if ((this.x <= px && px <= this.x + this.width) && (this.y <= py && py <= this.y + this.height)) {
+      console.log("-----------------------Triangle");
+      return true;
+    }
+    return false;
+  }
+}
