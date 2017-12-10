a5634a35106eb6ee82d3b6e8acedd02ccd79f424
hyejinim <hyejinim17@gmail.com>
2017-12-10 12:57:24 -0500

    Update Shape.js

--- Shape.js
+++ Shape.js
@@ -1,25 +1,29 @@
-class Shape {
-  constructor(type) {
-    this.type = type;
-    this.size = getRandomValue(70, 150);
-    this.speed = this.size / 20;
-    this.x = c.width + 50;
-    this.y = c.height * 3/5;
-    this.color = getRandomColor();
+class Trapezoid extends Shape {
+  constructor() {
+    super();
+    this.type = "trapezoid";
+    this.y = c.height * 3/5 + 15;
+    this.width = this.size;
+    this.height = this.size;
   }
-  setPosition(x, y) {
-    this.x = x;
-    this.y = y;
+  draw(context) {
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.moveTo(this.x + this.width*0.2, this.y);
+    context.lineTo(this.x, this.y + this.height);
+    context.lineTo(this.x + this.width, this.y + this.height);
+    context.lineTo(this.x + this.width*0.8, this.y);
+    context.lineTo(this.x + this.width*0.3, this.y);
+    context.closePath();
+    context.fill();
+
+    this.drawOutliine(context);
   }
-  setSize(x) {
-    this.size = x;
-  }
-  tick() {
-    this.x -= 20;
-  }
-  drawOutliine(context) {
-    context.lineWidth = 5;
-    context.strokeStyle = '#000000';
-    context.stroke();
+  contains(px, py) {
+    if ((this.x <= px && px <= this.x + this.width) && (this.y <= py && py <= this.y + this.height)) {
+      console.log("-----------------------Rect");
+      return true;
+    }
+    return false;
   }
 }
