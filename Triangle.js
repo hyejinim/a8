50394cc33299df4046476da13723d0f10b1a8b50
hyejinim <hyejinim17@gmail.com>
2017-12-10 12:57:43 -0500

    Update Triangle.js

--- Triangle.js
+++ Triangle.js
@@ -6,15 +6,9 @@ class Triangle extends Shape {
     this.size = this.size * 1.5; // Adjust scale
     this.width = this.size; // for bounding box
     this.height = this.size * 3/4; // for bounding box
-    // this.width = getRandomValue(100, 200);
-    // this.height = getRandomValue(100, 200);
-    // console.log("make rect " + runningShapes.length);
   }
   draw(context) {
-    // super.draw();
     context.fillStyle = this.color;
-    context.beginPath();
-
     context.beginPath();
     context.moveTo(this.x, this.y);
     context.lineTo(this.x + this.width/2, this.y + this.height);
