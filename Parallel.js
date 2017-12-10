4a74582a097ab78e1b76dadbe6619d94df8667e8
hyejinim <hyejinim17@gmail.com>
2017-12-10 12:55:34 -0500

    Update Parallel.js

--- Parallel.js
+++ Parallel.js
@@ -1,24 +1,19 @@
-class Hexagon extends Shape {
+class Parallel extends Shape {
   constructor() {
     super();
-    this.type = "hexagon";
+    this.type = "parallelogram";
     this.y = c.height * 3/5 + 15;
     this.width = this.size;
     this.height = this.size;
   }
   draw(context) {
-    var facShort = 0.225;
-    var facLong = 1 - facShort;
-
     context.fillStyle = this.color;
     context.beginPath();
-    context.moveTo(this.x + this.width*0.5, this.y);
-    context.lineTo(this.x, this.y + this.height*facShort);
-    context.lineTo(this.x, this.y + this.height*facLong);
-    context.lineTo(this.x + this.width*0.5, this.y + this.height);
-    context.lineTo(this.x + this.width, this.y + this.height*facLong);
-    context.lineTo(this.x + this.width, this.y + this.height*facShort);
-    context.lineTo(this.x + this.width*0.5, this.y);
+    context.moveTo(this.x + this.width*0.3, this.y);
+    context.lineTo(this.x, this.y + this.height);
+    context.lineTo(this.x + this.width*0.7, this.y + this.height);
+    context.lineTo(this.x + this.width, this.y);
+    context.lineTo(this.x + this.width*0.3, this.y);
     context.closePath();
     context.fill();
 
