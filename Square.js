6655648a57c46a16eb97cf33cca2cc0c8f878394
hyejinim <hyejinim17@gmail.com>
2017-12-10 16:36:27 -0500

    Add files via upload

---
+++ Square.js
@@ -0,0 +1,26 @@
+class Square extends Shape {
+  constructor() {
+    super();
+    this.type = "square";
+    this.y = c.height * 3/5 + 15;
+    this.width = this.size;
+    this.height = this.size;
+  }
+  draw(context) {
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.fillRect(this.x, this.y, this.width, this.width);
+
+    // the outline
+    context.lineWidth = 5;
+    context.strokeStyle = '#000000';
+    context.strokeRect(this.x, this.y, this.width, this.width);
+  }
+  contains(px, py) {
+    if ((this.x <= px && px <= this.x + this.width) && (this.y <= py && py <= this.y + this.height)) {
+      console.log("collided with Square");
+      return true;
+    }
+    return false;
+  }
+}
