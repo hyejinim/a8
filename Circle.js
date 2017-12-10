55e1d73c6cccfc4da2cbf11e578873a0348d4723
hyejinim <hyejinim17@gmail.com>
2017-12-10 12:53:19 -0500

    Update Circle.js

--- Circle.js
+++ Circle.js
@@ -2,14 +2,10 @@ class Circle extends Shape {
   constructor() {
     super();
     this.type = "circle";
-    // this.x = c.width + 50;
     this.y = c.height * 3/5 + 50; // Adjust circle's y position
     this.r = this.size/2;
-    // this.w = getRandomValue(10, 20);
-    // console.log("make circle " + runningShapes.length);
   }
   draw(context) {
-    // super.draw();
     context.fillStyle = this.color;
     context.beginPath();
     context.arc (this.x, this.y, this.r, 0, Math.PI * 2);
@@ -19,7 +15,6 @@ class Circle extends Shape {
   }
   contains(px, py) {
     if (Math.sqrt((px-this.x)*(px-this.x) + (py-this.y)*(py-this.y)) < this.r) {
-      console.log("-----------------------Circle");
       return true;
     }
     return false;
