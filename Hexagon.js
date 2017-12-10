439b80afcdae7fa84b302f077ea665b79cc4b4f6
hyejinim <hyejinim17@gmail.com>
2017-12-10 12:55:03 -0500

    Update Hexagon.js

--- Hexagon.js
+++ Hexagon.js
@@ -7,12 +7,10 @@ class Hexagon extends Shape {
     this.height = this.size;
   }
   draw(context) {
-    // super.draw();
-    context.fillStyle = this.color;
     var facShort = 0.225;
     var facLong = 1 - facShort;
 
-
+    context.fillStyle = this.color;
     context.beginPath();
     context.moveTo(this.x + this.width*0.5, this.y);
     context.lineTo(this.x, this.y + this.height*facShort);
@@ -25,13 +23,9 @@ class Hexagon extends Shape {
     context.fill();
 
     this.drawOutliine(context);
-
-
   }
   contains(px, py) {
-    // console.log("rectan contains")
     if ((this.x <= px && px <= this.x + this.width) && (this.y <= py && py <= this.y + this.height)) {
-      console.log("-----------------------Rhombus");
       return true;
     }
     return false;
