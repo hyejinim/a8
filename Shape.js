900491ee49abbf75db40e58246e0817306252d79
hyejinim <hyejinim17@gmail.com>
2017-12-10 02:26:27 -0500

    Update Shape.js

--- Shape.js
+++ Shape.js
@@ -1,79 +1,57 @@
-class Shape {
-  constructor(type) {
+class Gun {
+
+  constructor(player, type) {
     this.type = type;
-    this.size = getRandomValue(70, 150); // radious for circle, width/height for rect
-    // this.width = getRandomValue(70, 150);
-    // this.height = getRandomValue(70, 150);
-    this.speed = this.size / 20;
-    this.x = c.width + 50;
-    this.y = c.height * 3/5;
-    this.color = getRandomColor();
-    // console.log("make shape " + runningShapes.length);
+    this.text = type + " >>"; //▶
+    this.size = 30;
+    this.x = player.x + 200;
+    this.y = player.y + 120;
+    this.shooting = false;
+    this.width = gc.measureText(type).width;
+    this.height = gc.measureText(type).height;
+    // this.hit = false;
+  }
+  draw(context) {
+    context.fillStyle = "#000000";
+    context.font = (this.size) + "px Helvetica";
+    context.textAlign="right";
+    context.fillText(this.text, this.x, this.y);
   }
   setPosition(x, y) {
     this.x = x;
     this.y = y;
   }
-  setSize(x) {
-    this.size = x;
+  setShooting(isShot) {
+    this.shooting = isShot;
+    // this.hit = true;
+    // console.log();
   }
   tick() {
-    this.x -= 10;
-  }
+    if (this.shooting) {
+      // if (!this.hit) {
+        this.x += 20;
+        // console.log('shot tick')
+        // this.shot = false;
+      }
+      else {
 
-}
-
-class Circle extends Shape {
-  constructor() {
-    super();
-    this.type = "circle";
-    // this.x = c.width + 50;
-    this.y = c.height * 3/5 + 50; // Adjust circle's y position
-    this.r = this.size/2;
-    // this.w = getRandomValue(10, 20);
-    // console.log("make circle " + runningShapes.length);
-  }
-  draw(context) {
-    gc.fillStyle = this.color;
-    context.beginPath();
-    context.arc (this.x, this.y, this.r, 0, Math.PI * 2);
-    context.fill();
-    // console.log("draw circle " + runningShapes.length);
-  }
-  contains(px, py) {
-    console.log("circle contains")
-    if (Math.pow(px-this.x, this.r) + Math.pow(py - this.y, this.r) >= this.r * this.r) {
-      console.log("-----------------------Circle");
-      return true;
+      }
     }
-    return false;
   }
 
-}
 
-class Rectangle extends Shape {
-  constructor() {
-    super();
-    this.type = "rectangle";
-    this.y = c.height * 3/5 + 15;
-    this.width = this.size; // for bounding box
-    this.height = this.size; // for bounding box
-    // this.width = getRandomValue(100, 200);
-    // this.height = getRandomValue(100, 200);
-    // console.log("make rect " + runningShapes.length);
-  }
-  draw(context) {
-    gc.fillStyle = this.color;
-    context.beginPath();
-    context.fillRect(this.x, this.y, this.size, this.size);
-    // console.log("draw rect " + runningShapes.length);
-  }
-  contains(px, py) {
-    console.log("rectan contains")
-    if ((this.x <= px && px <= this.x + this.width) && (this.y <= py && py <= this.y + this.height)) {
-      console.log("-----------------------Rect");
-      return true;
-    }
-    return false;
-  }
-}
+// if (this.jumped) {
+//   // Jumping animation
+//   if (this.y = (c.height-15) * 2.3/4) {
+//     this.image.src = 'images/monkey_jump_2.png';
+//     this.y = this.y - 300;
+//     console.log("player position: ", this.y);
+//   }
+//   this.jumped = false;
+// } else {
+//   if (this.y < (c.height-15) * 2.3/4) {
+//     this.y = this.y + 20;
+//     console.log("player position: ", this.y);
+//     this.image.src = 'images/monkey_jump_2.png';
+//   }
+// }
