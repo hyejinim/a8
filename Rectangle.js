81eeee0f1c7c9504a2d56a193f7d9a133a647ef8
hyejinim <hyejinim17@gmail.com>
2017-12-10 12:56:49 -0500

    Update Rectangle.js

--- Rectangle.js
+++ Rectangle.js
@@ -7,11 +7,9 @@ class Rectangle extends Shape {
     this.height = this.size;
   }
   draw(context) {
-    // super.draw();
     context.fillStyle = this.color;
     context.beginPath();
     context.fillRect(this.x, this.y, this.size, this.size);
-    // console.log("draw rect " + runningShapes.length);
 
     // the outline
     context.lineWidth = 5;
@@ -19,7 +17,6 @@ class Rectangle extends Shape {
     context.strokeRect(this.x, this.y, this.size, this.size);
   }
   contains(px, py) {
-    // console.log("rectan contains")
     if ((this.x <= px && px <= this.x + this.width) && (this.y <= py && py <= this.y + this.height)) {
       console.log("-----------------------Rect");
       return true;
