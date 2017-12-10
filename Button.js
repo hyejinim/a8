f7164ad60042f78f462518d09cd1e768d55e178e
hyejinim <hyejinim17@gmail.com>
2017-12-10 17:16:41 -0500

    Update Button.js

--- Button.js
+++ Button.js
@@ -2,9 +2,14 @@ class Button {
   constructor(label) {
     this.button = document.createElement("input");
     this.button.setAttribute("type", "button");
-    this.button.setAttribute("value", label);
-    this.button.setAttribute("id", "button");
+    this.button.setAttribute("value", label);  
     document.getElementById("buttonarea").appendChild(this.button);
+
+    if (label == "Start") {
+      this.button.setAttribute("id", "button");
+    }else {
+      this.button.setAttribute("id", "button2");
+    }
 
     var self = this;
     this.button.onclick = function() {
@@ -26,6 +31,12 @@ class Button {
     } else if (label == "Stop") {
       gameStart = false;
       clearInterval(this.shapeTimerID);
+    } else if (label == "Restart") {
+      init();
+      gameStart = true;
+      this.shapeTimerID = setInterval(launchShape, 4000); // launch a new object every 2000 msec (2sec)
+      tick();
+      this.button.blur();
     }
   }
 }
