4a68756e00498c4445069c4e983dcfd1dc7b7600
hyejinim <hyejinim17@gmail.com>
2017-12-10 12:55:24 -0500

    Update Parallel.js

--- Parallel.js
+++ Parallel.js
@@ -1,33 +1,33 @@
-class Parallel extends Shape {
+class Hexagon extends Shape {
   constructor() {
     super();
-    this.type = "parallelogram";
+    this.type = "hexagon";
     this.y = c.height * 3/5 + 15;
     this.width = this.size;
     this.height = this.size;
   }
   draw(context) {
-    // super.draw();
+    var facShort = 0.225;
+    var facLong = 1 - facShort;
+
     context.fillStyle = this.color;
     context.beginPath();
-
-    context.moveTo(this.x + this.width*0.3, this.y);
-    context.lineTo(this.x, this.y + this.height);
-    context.lineTo(this.x + this.width*0.7, this.y + this.height);
-    context.lineTo(this.x + this.width, this.y);
-    context.lineTo(this.x + this.width*0.3, this.y);
+    context.moveTo(this.x + this.width*0.5, this.y);
+    context.lineTo(this.x, this.y + this.height*facShort);
+    context.lineTo(this.x, this.y + this.height*facLong);
+    context.lineTo(this.x + this.width*0.5, this.y + this.height);
+    context.lineTo(this.x + this.width, this.y + this.height*facLong);
+    context.lineTo(this.x + this.width, this.y + this.height*facShort);
+    context.lineTo(this.x + this.width*0.5, this.y);
     context.closePath();
     context.fill();
 
     this.drawOutliine(context);
-    console.log("draw PARALLEL");
   }
   contains(px, py) {
-    // console.log("rectan contains")
-    // if ((this.x <= px && px <= this.x + this.width) && (this.y <= py && py <= this.y + this.height)) {
-    //   console.log("-----------------------parallelogram");
-    //   return true;
-    // }
-    // return false;
+    if ((this.x <= px && px <= this.x + this.width) && (this.y <= py && py <= this.y + this.height)) {
+      return true;
+    }
+    return false;
   }
 }
